2cf3ac1796bd529991682192a189cf0d6fc4bdeb
admin <yonex12360@gmail.com>
2026-05-11 17:34:52 +0700

    port

--- lib/seo.ts
+++ lib/seo.ts
@@ -14,5 +14,5 @@ export function getBaseUrl() {
       // Fallback to local URL when env value is invalid
     }
   }
-  return new URL("http://localhost:3515");
+  return new URL(`http://localhost:${process.env.PORT ?? 3000}`);
 }
